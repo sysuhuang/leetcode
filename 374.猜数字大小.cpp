5c269860aa84ead25288b7fa325be8d40cf5eca0
sysuhuang <sysuhuang@outlook.com>
2020-03-30 09:09:08 +0800

    easy

---
+++ 374.猜数字大小.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=374 lang=cpp
+ *
+ * [374] 猜数字大小
+ *
+ * https://leetcode-cn.com/problems/guess-number-higher-or-lower/description/
+ *
+ * algorithms
+ * Easy (42.97%)
+ * Likes:    53
+ * Dislikes: 0
+ * Total Accepted:    17.7K
+ * Total Submissions: 41K
+ * Testcase Example:  '10\n6'
+ *
+ * 我们正在玩一个猜数字游戏。 游戏规则如下：
+ * 我从 1 到 n 选择一个数字。 你需要猜我选择了哪个数字。
+ * 每次你猜错了，我会告诉你这个数字是大了还是小了。
+ * 你调用一个预先定义好的接口 guess(int num)，它会返回 3 个可能的结果（-1，1 或 0）：
+ * 
+ * -1 : 我的数字比较小
+ * ⁠1 : 我的数字比较大
+ * ⁠0 : 恭喜！你猜对了！
+ * 
+ * 
+ * 示例 :
+ * 
+ * 输入: n = 10, pick = 6
+ * 输出: 6
+ * 
+ */
+
+// @lc code=start
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int left = 1, right = n;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            int flag = guess(mid);
+            if(flag == 0)
+                return mid;
+            if(flag == -1)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return -1;    
+    }
+
+};
+// @lc code=end
+
